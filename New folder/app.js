9ff868dec13142c1e6b7a45d50d8d29e210cb075
BilalQamar63 <143898156+BilalQamar63@users.noreply.github.com>
2023-09-17 12:13:57 +0500

    work done

---
+++ New folder/app.js
@@ -0,0 +1,133 @@
+// **********************Chapter no:1 ********Q1*********************
+
+// alert("Welcome to JS");
+
+// **********************Chapter no:1 ********Q2*********************
+
+// alert("Error! please enter a valid password");
+
+
+// **********************Chapter no:1 ********Q3*********************
+
+// alert("Welcome to JS land.....\n Happy coding");
+
+// **********************Chapter no:1 ********Q4*********************
+
+// alert("Welcome to JS land....");
+// alert("Happy coding");
+
+
+// **********************Chapter no:1 ********Q5*********************
+
+
+// console.log("hello i will run JS");
+
+// **********************Chapter no:2 ********Q1*********************
+
+// var username;
+
+
+// **********************Chapter no:2 ********Q2*********************
+
+// var myName = "Bilal Qamar"
+// console.log(myName);
+
+// **********************Chapter no:2 ********Q3*********************
+
+// var message = "hello world"
+// console.log(message)
+
+// **********************Chapter no:2 ********Q4*********************
+
+// var studentName = "abc";
+// var studentAge = "20";
+// var studentQualification = "Web and Mobile App";
+
+// alert(studentName);
+
+// alert(studentAge);
+
+// alert(studentQualification);
+
+
+// **********************Chapter no:2 ********Q5*********************
+
+
+// var pizza = "Pizza \n Pizz \n Piz \n Pi \n P"
+// alert(pizza)
+
+// **********************Chapter no:2 ********Q6*********************
+
+// var email = "abc@abc.com";
+// alert("my email adress is this  " + email);
+
+// **********************Chapter no:2 ********Q7*********************
+
+
+// var book = "The Smart Way to learn JavaScript";
+// alert("I am trying to leaarn from the book " + book)
+
+// **********************Chapter no:2 ********Q8*********************
+
+
+// document.write("  Yah ! I can write html content through JavaScript");
+
+
+// **********************Chapter no:2 ********Q9*********************
+
+
+// **********************Chapter no:2 ********Q10*********************
+
+// var age = 20
+// alert ("I am " + age + "  year old")
+
+// **********************Chapter no:2 ********Q11*********************
+
+// var timer = 14;
+// alert("you have visited this site " + timer + " times")
+
+// **********************Chapter no:2 ********Q12*********************
+
+
+// var birthYear=20;
+// document.write("My birth year is " + birthYear + "  <br>" +
+// "Data type of my declare variable is " + typeof(birthYear));
+
+// **********************Chapter no:2 ********Q13*********************
+
+// var visitorName = prompt("Enter your name");
+// var product ="T - shirt (s)";
+// var qty = +prompt("Enter Quantity");
+
+// document.write(visitorName + " ordered  " + qty +  product +"   on XYZ clothing Store ");
+
+// **********************Chapter no:5 ********Q1*********************
+
+// var num1 = 3;
+// var num2 = 5;
+// var add = num1 + num2;
+
+// document.write("Sum of " + num1 + "  and  " +  num2 + " is "+  add)
+
+
+// **********************Chapter no:5 ********Q2*********************
+
+
+// Subtraction
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var sub = num1 - num2;
+
+// document.write("Subtract of " + num1 + "  and  " +  num2 + " is "+  sub);
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var multiply = num1 * num2;
+
+// document.write("Sum of " + num1 + "  and  " +  num2 + " is "+  multiply)
+
+
+
